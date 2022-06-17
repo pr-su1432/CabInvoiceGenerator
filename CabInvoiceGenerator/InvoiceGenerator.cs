bae549f98c4ecc1d9d401beea9aacf515872f38d
[prasanna] <prasanna>
2022-06-17 12:21:30 +0530

    [prasanna]: by using distance and time calculating fare

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    internal class InvoiceGenerator
+    {
+        private readonly int COST_PER_MINUTE = 1;
+        private readonly double MINIMUM_FARE = 5;
+        private readonly double MINIMUM_COST_PER_KM = 10;
+        public int time;
+        public double distance;
+       
+        public InvoiceGenerator(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+        public double CalculateFare()
+        {
+            double totalFare = 0;
+            if (distance >= 0 && time >= 0)
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_MINUTE;
+            }
+            else
+            {
+                if (distance <= 0)
+                {
+                    throw new InvoiceGeneratorExceptions(InvoiceGeneratorExceptions.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if (time < 0)
+                {
+                    throw new InvoiceGeneratorExceptions(InvoiceGeneratorExceptions.ExceptionType.INVALID_MINUTE, "Invalid Time");
+                }
+            }
+            return totalFare;
+        }
+    }
+}
---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter distance you want to Ride");
+            int fare1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter time of your journey in minutes");
+            int fare2 = Convert.ToInt32(Console.ReadLine());
+            InvoiceGenerator example = new InvoiceGenerator(fare1, fare2);
+            double fare = example.CalculateFare();
+            Console.WriteLine("Fare: " + fare);
+
+        }
+    }
+}
